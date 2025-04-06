45decf99fbc13cf6464d09fb33e730b17112e988
Konrad <kondykox@gmail.com>
2025-04-06 17:18:57 +0200

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center items-center flex-column">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-bold p-2 text-gradient">
+        Tierlista Idoli Kondiego
+      </h1>
+    </main>
+  );
+}
+
+export default App;
